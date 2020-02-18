5a1c4e4517a06083558a2d377453a7ea724ceaed
Ross-Marshall <rossmarshallgithub@gmail.com>
2020-02-17 22:34:34 -0500

    Add files via upload

---
+++ python_examples/if_then.py
@@ -0,0 +1,7 @@
+# Collected string / test length
+
+input = raw_input("Please enter a test string: ")
+
+if len(input) < 6:
+  print("Your string is too short.")
+  print("Please enter a string with at least characters.")
